0842b1b8c6afa32c076d9807afe3c84eae68b960
Bruno <btisocco@gmail.com>
2020-06-13 13:09:54 -0300

    asdasdasdsa

--- detail.php
+++ detail.php
@@ -7,19 +7,19 @@ require '/app/vendor/autoload.php';
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"); // Either Production or SandBox AccessToken
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
+
 $preference_data = array(
     "external_reference" => $transaction_id,
     "back_urls"=> array (
-        "success"=> 'https://btisocco-mp-commerce-php.herokuapp.com/success',
-        "failure"=> 'https://btisocco-mp-commerce-php.herokuapp.com/failure',
-        "pending"=> 'https://btisocco-mp-commerce-php.herokuapp.com/pending'
+        "success"=> 'http://www.midominio.com/transaccion_exitosa',
+        "failure"=> 'http://www.midominio.com/transaccion_erronea'
     ),						
     "items" => array(
             array(
             "title" =>'Producto1',
             "quantity" => 1,
             "currency_id" => "ARS",
-            "unit_price" => "(float) round($precio,2) "
+            "unit_price" => (float) round($precio,2) 
         )
         ),
     "payment_methods" => array( /*aqui excluyo tipos de pago rapipago, transf bancaria, atm*/
