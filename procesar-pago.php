b4983e0e32aa3d32e2455c3061389cfaed904dd2
Bruno <btisocco@gmail.com>
2020-06-13 14:53:21 -0300

    asd

--- procesar-pago.php
+++ procesar-pago.php
@@ -1,3 +1,5 @@
 <?php
+MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-echo 'Procesar Pago';
+$info = MercadoPago\Preference::find_by_id($MP_id_payment);
+echo "<pre>"; print_r($info);
