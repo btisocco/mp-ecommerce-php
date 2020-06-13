2cc97cbec90c9afaaf184e57d099f516cd5e017f
Bruno <btisocco@gmail.com>
2020-06-13 12:29:36 -0300

    cambios

--- detail.php
+++ detail.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once 'vendor/autoload.php'; // You have to require the library from your Composer vendor folder
+// require_once 'vendor/autoload.php'; // You have to require the library from your Composer vendor folder
 
-// require '/app/vendor/autoload.php';
+require '/app/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"); // Either Production or SandBox AccessToken
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
@@ -11,11 +11,11 @@ MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 $preference = new MercadoPago\Preference();
 
 //payment_methods
-$preference->$payment_methods = new MercadoPago\Payment_methods();
-$preference->$payment_methods->excluded_payment_methods = array("id"=>"amex");
-$preference->$payment_methods->excluded_payment_types = array("id"=>"atm");
-$preference->$payment_methods->installments=6;
-// $preference->payment_methods = $payment_methods;
+$payment_methods = new MercadoPago\Payment_methods();
+$payment_methods->excluded_payment_methods = array("id"=>"amex");
+$payment_methods->excluded_payment_types = array("id"=>"atm");
+$payment_methods->installments=6;
+$preference->payment_methods = $payment_methods;
 
 //backs_url
 $preference->back_urls = array(
@@ -168,10 +168,12 @@ $preference->save();
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <script
+                                    <form action="/procesar-pago.php" method="POST">
+                                        <script
                                         src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
                                         data-preference-id="<?php echo $preference->id; ?>">
-                                    </script>
+                                        </script>
+                                    </form>
                                     <!-- <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button> -->
                                 </div>
                             </div>
