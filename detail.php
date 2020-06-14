c94a27047e25e461be9993a0ad1cbb8968ae23f0
Bruno <btisocco@gmail.com>
2020-06-14 09:11:31 -0300

    asd

--- detail.php
+++ detail.php
@@ -198,6 +198,7 @@ $preference->save();
                                         </h3>
                                     </div>
                                     <a href="<?php echo $preference->init_point; ?>" class="mercadopago-button">Pagar la Compra</a>
+                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
                                 </div>
                             </div>
                         </div>
